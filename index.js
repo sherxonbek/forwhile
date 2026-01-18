c261a870e9bc7225d7daefa2dfe40c3e27ba2b6f
Sherzodbek <148542339+sherxonbek@users.noreply.github.com>
2026-01-18 17:32:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+////for dasrlik masalasi////
+//1-masala
+
+/*let k = Number(prompt("K Son kiriting:"));
+let n = Number(prompt("N Necha marta chaqirilsin:"));
+
+if(n > 0){
+    for(let i = 1; i <= n; i++){
+        console.log(k);
+    }
+}else{
+    console.log("N manfiy son bo'lishi mumkin emas");
+}*/
+
+//2-masala
+
+/*let kanfetNarxi = Number(prompt("Kanfet narxini kiriting:"));
+
+for (let i = 0.1; i <= 1; i += 0.1) {
+    let narx = kanfetNarxi * i;
+    console.log(narx);
+}*/
+
+//3-masala
+
+/*let n = Number(prompt("N sonini kiriting:"));
+let yigindi = 0;
+
+if (n > 0) {
+    for (let i = n; i <= 2 * n; i++) {
+        yigindi += i*i;
+    }
+    console.log(yigindi);
+} else {
+    console.log("N manfiy son bo`lmasligi kerak");
+}*/
+
+//4-masala
+// let N =5;
+// let yigindi =0;
+
+// for(let i =1; i <= N; i++){
+//     yigindi +=i**(N-i+1);
+// }
+// console.log(yigindi);
+
+//5-masala
+
+// let A = Number(prompt("A sonini kiriting:"));
+// let B = Number(prompt("B sonini kiriting:"));
+
+// if (A < B) {
+//     for (let i = A; i <= B; i++) {
+//         for (let j = 1; j <= i; j++) {
+//             console.log(i);
+//         }
+//     }
+// } else {
+//     console.log("A soni B sonidan kichik bo'lishi kerak");
+// }
+
+//6-masala
+// let N = Number(prompt("N sonini kiriting:"));
+// if (N > 0) {
+//     for (let i = 1; i <= N; i++) {
+//         let natija = N % i == 0;
+//         if (natija) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+//7-masala
+// let N = Number(prompt("N sonini kiriting:"));
+// let K = Number(prompt("K sonini kiriting:"));
+// let butun = 0;
+
+// while (N>= K) {
+//     N -= K;
+//     butun++;
+// }
+// console.log("Butun qismi: " + butun);
+// console.log("Qoldiq: " + N);
+
+//8-masala
+// let n = Number(prompt("n sonini kiriting:"));
+// let k = 0; 
+// while (n > 1) {
+//     n = n / 2; 
+//     k++;              
+// }
+// console.log(k);
+
